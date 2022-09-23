c9f960a693985305e41ff842cc9844579b09252c
ChankitSingh <82271249+ChankitSingh@users.noreply.github.com>
2022-09-24 00:57:26 +0530

    Create graphusingadjlist.cpp

---
+++ graphusingadjlist.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<unordered_map>
+#include<list>
+using namespace std;
+
+class graph{
+    public:
+        unordered_map<int,list<int>> adj;
+        
+        void addedge(int u, int v, bool direction){
+            adj[u].push_back(v);
+            
+            if(direction == 0){
+                adj[v].push_back(u);
+            }
+        }
+        void printadjlist(){
+            for(auto i:adj){
+                cout<< i.first<<"->";
+                for(auto j:i.second){
+                cout<<j<<",";
+            }
+            cout<<endl;
+        }
+        }
+};
+
+int main() {
+    int n;
+    cout<<"enter no of edges"<<endl;
+    cin>>n;
+    
+    int m;
+    cout<<"enter th no of edges"<<endl;
+    cin>>m;
+    
+    graph g;
+    
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        //undirected graph
+        g.addedge(u,v,0);
+    }
+    
+    g.printadjlist();
+    return 0;
+}
